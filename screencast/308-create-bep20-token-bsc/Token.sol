ad94a1c7959286c35cfe46b44c823f1c3903c844
Technology Experts <aymanfarajallah0@gmail.com>
2026-01-08 01:57:14 +0200

    Update totalSupply to 1 billion tokens

--- screencast/308-create-bep20-token-bsc/Token.sol
+++ screencast/308-create-bep20-token-bsc/Token.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.2;
 contract Token {
     mapping(address => uint) public balances;
     mapping(address => mapping(address => uint)) public allowance;
-    uint public totalSupply = 10000 * 10 ** 18;
+    uint public totalSupply = 1000000000* 10 ** 18;
     string public name = "My Token";
     string public symbol = "TKN";
     uint public decimals = 18;
